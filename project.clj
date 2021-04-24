598a1e4b879bc354b87402e4ffd0fcaaa41732b4
Thiago Kenji Okada <thiagokokada@gmail.com>
2021-04-23 23:59:17 -0300

    Update deps

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "docopt creates beautiful command-line interfaces - clojure port"
   :url "http://docopt.org"
   :license {:name "MIT" :url "https://github.com/docopt/docopt.clj/blob/master/LICENSE"}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
-  :profiles {:test {:dependencies [[org.clojure/data.json "0.2.1"]]}}
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :profiles {:test {:dependencies [[org.clojure/data.json "2.2.1"]]}}
   :aot :all)
