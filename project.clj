5eaafed71954ee836f6b0a0d997865f1cb1bae9d
Thiago Kenji Okada (Nubank) <thiago.okada@nubank.com.br>
2021-05-06 10:26:47 -0300

    Add project ns and bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject docopt "0.6.1"
+(defproject dev.nubank/docopt "0.6.1-fix2"
   :description "docopt creates beautiful command-line interfaces - clojure port"
   :url "http://docopt.org"
   :license {:name "MIT" :url "https://github.com/docopt/docopt.clj/blob/master/LICENSE"}
