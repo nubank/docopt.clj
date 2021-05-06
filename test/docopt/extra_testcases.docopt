nubank/docopt.clj

commit d2413b2ec95203fbc452ef4fbfe4d0294151dfc0

Ignore "--" during usage block parsing
This is parsing as an option (not sure if `:long-option` or
`:short-option`, however it appears in result and it is never true).

So create its own token and ignore it.

As you can see in the "Complex command" test case, this is still wrong
in one more complex case. But at least it accepts the grammar now
(before it outright rejected it).

src/docopt/usageblock.clj
@@ -48,7 +48,8 @@
                      [#"(\)|\])"                           ::end-group]]
                     (map vector longs-re            (repeat :long-option)) 
                     (map vector shorts-re           (repeat :short-options))
-                    [[(re-tok "--([^= ]+)=(<[^<>]*>|\\S+)") :long-option]
+                    [[(re-tok "--")                         :args-separator]
+                     [(re-tok "--([^= ]+)=(<[^<>]*>|\\S+)") :long-option]
                      [(re-tok "--(\\S+)")                   :long-option]
                      [(re-tok "-(?!-)(\\S+)")               :short-options]
                      [(re-tok re-arg-str)                  ::argument]
@@ -68,11 +69,12 @@
   "Adds line number to usage token, and replaces option name(s) with option object(s)."
   [[tag name arg :as token] lnum options] 
   tag
-  ::token        [(into [tag lnum] (rest token))]
-  :long-option   [(find-option :long name arg lnum options)]
-  :short-options (letfn [(new-short [arg c] (find-option :short (str c) arg lnum options))]
-                   (conj (into [] (map (partial new-short nil) (butlast name)))
-                         (new-short arg (last name)))))
+  ::token         [(into [tag lnum] (rest token))]
+  :args-separator [] ;; ignore
+  :long-option    [(find-option :long name arg lnum options)]
+  :short-options  (letfn [(new-short [arg c] (find-option :short (str c) arg lnum options))]
+                    (conj (into [] (map (partial new-short nil) (butlast name)))
+                          (new-short arg (last name)))))
 
 (defn tokenize-pattern-lines
   "Helper function for 'tokenize-patterns'."

test/docopt/extra_testcases.docopt
@@ -1,0 +1,36 @@
+r"""Usage: prog foo -- <extra-opts>...
+
+"""
+$ prog foo -- --bar
+{"<extra-opts>":["--bar"],"foo":true}
+
+
+r"""Complex command
+
+Usage:
+    prog [options] <param-x> <param-y> -- <extra>...
+    prog [options] <param-a> <param-b> <param-c> <param-d> -- <extra>...
+    prog [options] <param-x> <param-y>
+    prog [options] <param-a> <param-b> <param-c> <param-d>
+
+Options:
+    -f --foo     Foo
+    --bar <bar>  Bar
+
+"""
+
+$ prog x y --foo
+{"--bar":null,"--foo":true,"<extra>":[],"<param-a>":null,"<param-b>":null,"<param-c>":null,"<param-d>":null,"<param-x>":"x","<param-y>":"y"}
+
+$ prog a b c d
+{"--bar":null,"--foo":false,"<extra>":["c","d"],"<param-a>":null,"<param-b>":null,"<param-c>":null,"<param-d>":null,"<param-x>":"a","<param-y>":"b"}
+
+$ prog a b c d --foo --bar bar
+{"--bar":"bar","--foo":true,"<extra>":[],"<param-a>":"a","<param-b>":"b","<param-c>":"c","<param-d>":"d","<param-x>":null,"<param-y>":null}
+
+$ prog x y --bar bar -- extra
+{"--bar":"bar","--foo":false,"<extra>":["extra"],"<param-a>":null,"<param-b>":null,"<param-c>":null,"<param-d>":null,"<param-x>":"x","<param-y>":"y"}
+
+# This is wrong, `<extra>` should only have "extra"
+$ prog a b c d --foo --bar bar -- extra
+{"--bar":"bar","--foo":true,"<extra>":["c","d","extra"],"<param-a>":null,"<param-b>":null,"<param-c>":null,"<param-d>":null,"<param-x>":"a","<param-y>":"b"}

test/docopt/core_test.clj
@@ -38,7 +38,7 @@
       (let [result (or (m/match-argv docinfo in) "user-error")]
         (if (not= result out)
           (str "\n" (s/trim-newline doc) "\n$ prog " (s/join " " in) 
-               "\nexpected: " out "\nobtained: " result "\n\n"))))))
+               "\nexpected: " (json/write-str out) "\nobtained: " (json/write-str result) "\n\n"))))))
 
 (defn valid?
   "Validates all test cases found in the file named 'test-cases-file-name'."
@@ -51,4 +51,5 @@
     true))
 
 (deftest docopt
-  (is (valid? "https://raw.github.com/docopt/docopt/511d1c57b59cd2ed663a9f9e181b5160ce97e728/testcases.docopt")))
+  (is (valid? "https://raw.github.com/docopt/docopt/511d1c57b59cd2ed663a9f9e181b5160ce97e728/testcases.docopt"))
+  (is (valid? "test/docopt/extra_testcases.docopt")))

commit 882d009f641c9ea21026c4c18a5d1a125cb0b716

Better fix the issue with `--`
This is a partial revert of commits:
- d2413b2ec95203fbc452ef4fbfe4d0294151dfc0 (ignore `--` in
  docopt.usageblock/tokenize-pattern)
- 6845163cbdd1ba8756905d5ac1b97de85811f603 (find biggest-match in
  docopt.match/match-argv)

The actual root cause of the issue while parsing `--` was the
`docopt.match/parse` function. `docopt.usageoption/parse` seems correct,
at least while parsing this:

```
(parse "usage: prog foo -- <extra-opts>...")

:docopt.usageblock/required
[ :docopt.usageblock/command "foo" ]
[ :docopt.usageblock/command "--" ]
[ :docopt.usageblock/repeat [ :docopt.usageblock/argument "<extra-opts>" ] ]
```

As it is clear above, `--` is correctly parsed as a
`:docopt.usageblock/command`. This is the same behavior as
docopt/docopt, but commit d2413b2ec95203fbc452ef4fbfe4d0294151dfc0 broke
this.

Reverting commit d2413b2ec95203fbc452ef4fbfe4d0294151dfc0 and now the
issue is in `docopt.match/parse`, since it was ignoring `--` thanks to
the `[head _ & tail] (partition-by (partial "--") argv)` part. Removing
`_` and putting `--` at the tail seems to do the trick.

Compared to the previous trick, this fix some tests and breaks other
ones. But I think the code as a whole now is more correctly, and the
other issues can be fixed by other commits.

src/docopt/match.clj
@@ -19,7 +19,7 @@
 (defn parse
   "Parses the command-line arguments into a matchable state [acc remaining-option-values remaining-words]."
   [{:keys [acc shorts-re longs-re]} argv]
-  (let [[head _ & tail] (partition-by (partial = "--") argv)
+  (let [[head & tail]   (partition-by (partial = "--") argv)
         options         (remove string? (keys acc))
         tokens          (mapcat #(expand % options) (tokenize (s/join " " head) 
                                                               (concat (map vector longs-re  (repeat :long-option))
@@ -71,15 +71,9 @@
      (= [] value) (if (vector? default-value) default-value [default-value])
      :else        value)])
 
-(defn- biggest-match
-  [matches]
-  (letfn [(filled-vals-count [match]
-            (->> match first vals (remove nil?) count))]
-    (->> matches (sort-by filled-vals-count >) first)))
-
-(defn match-argv 
+(defn match-argv
   "Match command-line arguments with usage patterns."
   [docmap argv]
   (if-let [state (parse docmap argv)]
-    (if-let [[match] (biggest-match (filter #(every? empty? (cons (% 2) (vals (% 1)))) (matches #{state} (:tree docmap))))]
+    (if-let [[match] (first (filter #(every? empty? (cons (% 2) (vals (% 1)))) (matches #{state} (:tree docmap))))]
       (into (sorted-map) (map #(if (string? (key %)) % (option-value %)) match)))))

src/docopt/usageblock.clj
@@ -48,8 +48,7 @@
                      [#"(\)|\])"                           ::end-group]]
                     (map vector longs-re            (repeat :long-option)) 
                     (map vector shorts-re           (repeat :short-options))
-                    [[(re-tok "--")                         :args-separator]
-                     [(re-tok "--([^= ]+)=(<[^<>]*>|\\S+)") :long-option]
+                    [[(re-tok "--([^= ]+)=(<[^<>]*>|\\S+)") :long-option]
                      [(re-tok "--(\\S+)")                   :long-option]
                      [(re-tok "-(?!-)(\\S+)")               :short-options]
                      [(re-tok re-arg-str)                  ::argument]
@@ -70,7 +69,6 @@
   [[tag name arg :as token] lnum options] 
   tag
   ::token         [(into [tag lnum] (rest token))]
-  :args-separator [] ;; ignore
   :long-option    [(find-option :long name arg lnum options)]
   :short-options  (letfn [(new-short [arg c] (find-option :short (str c) arg lnum options))]
                     (conj (into [] (map (partial new-short nil) (butlast name)))

test/docopt/extra_testcases.docopt
@@ -1,9 +1,30 @@
+# Should output the same things as docopt/docopt for language agnostic tests
+
+# Testing `--`
+
 r"""Usage: prog foo -- <extra-opts>...
 
 """
-$ prog foo -- --bar
-{"<extra-opts>":["--bar"],"foo":true}
 
+$ prog foo
+"user-error"
+
+$ prog foo -- --bar
+{"--":true,"<extra-opts>":["--bar"],"foo":true}
+
+r"""Usage: prog foo [--] <extra-opts>...
+
+"""
+
+$ prog foo
+"user-error"
+# Wrong, should be
+# {"foo": true, "--": false, "<extra-opts>": []}
+
+$ prog foo -- --bar
+{"--":false,"<extra-opts>":["--","--bar"],"foo":true}
+# Wrong, should be
+# {"foo": true, "--": true, "<extra-opts>": ["--bar"]}
 
 r"""Complex command
 
@@ -20,21 +41,21 @@ Options:
 """
 
 $ prog x y --foo
-{"--bar":null,"--foo":true,"<extra>":[],"<param-a>":null,"<param-b>":null,"<param-c>":null,"<param-d>":null,"<param-x>":"x","<param-y>":"y"}
+{"--":false,"--bar":null,"--foo":true,"<extra>":[],"<param-a>":null,"<param-b>":null,"<param-c>":null,"<param-d>":null,"<param-x>":"x","<param-y>":"y"}
 
 $ prog a b c d
-{"--bar":null,"--foo":false,"<extra>":[],"<param-a>":"a","<param-b>":"b","<param-c>":"c","<param-d>":"d","<param-x>":null,"<param-y>":null}
+{"--":false,"--bar":null,"--foo":false,"<extra>":[],"<param-a>":"a","<param-b>":"b","<param-c>":"c","<param-d>":"d","<param-x>":null,"<param-y>":null}
 
 $ prog a b c d --foo --bar bar
-{"--bar":"bar","--foo":true,"<extra>":[],"<param-a>":"a","<param-b>":"b","<param-c>":"c","<param-d>":"d","<param-x>":null,"<param-y>":null}
+{"--":false,"--bar":"bar","--foo":true,"<extra>":[],"<param-a>":"a","<param-b>":"b","<param-c>":"c","<param-d>":"d","<param-x>":null,"<param-y>":null}
 
 $ prog x y --bar bar -- extra
-{"--bar":"bar","--foo":false,"<extra>":["extra"],"<param-a>":null,"<param-b>":null,"<param-c>":null,"<param-d>":null,"<param-x>":"x","<param-y>":"y"}
+{"--":false,"--bar":"bar","--foo":false,"<extra>":[],"<param-a>":"x","<param-b>":"y","<param-c>":"--","<param-d>":"extra","<param-x>":null,"<param-y>":null}
+# Wrong, should be
+# {"--bar": "bar", "--foo": false, "<param-x>": "x", "<param-y>": "y", "--": true, "<extra>": ["extra"], "<param-a>": null, "<param-b>": null, "<param-c>": null, "<param-d>": null}
 
 $ prog a b c d --foo --bar bar -- extra
-{"--bar":"bar","--foo":true,"<extra>":["extra"],"<param-a>":"a","<param-b>":"b","<param-c>":"c","<param-d>":"d","<param-x>":null,"<param-y>":null}
+{"--foo": true, "--bar": "bar", "<param-x>": null, "<param-y>": null, "--": true, "<extra>": ["extra"], "<param-a>": "a", "<param-b>": "b", "<param-c>": "c", "<param-d>": "d"}
 
 $ prog x y -- e1 e2 e3 e4
-{"--bar":null,"--foo":false,"<extra>":["e3","e4"],"<param-a>":"x","<param-b>":"y","<param-c>":"e1","<param-d>":"e2","<param-x>":null,"<param-y>":null}
-# Wrong, this should be:
-# {"--bar":null,"--foo":false,"<extra>":["e1","e2","e3","e4"],"<param-a>":null,<param-b>":null,<param-c>":null,"<param-d>":null,<param-x>":"x","<param-y>":"y"}
+{"--bar": null, "--foo": false, "<param-x>": "x", "<param-y>": "y", "--": true, "<extra>": ["e1", "e2", "e3", "e4"], "<param-a>": null, "<param-b>": null, "<param-c>": null, "<param-d>": null}