ceed53a4db6a1786a5f216aa313df06b107dc3bd
Thiago Kenji Okada <thiagokokada@gmail.com>
2021-05-05 23:48:30 -0300

    Lint fixes, improve documentation

--- src/docopt/core.clj
+++ src/docopt/core.clj
@@ -14,11 +14,31 @@
     (u/parse (section "usage" s/split-lines) (o/parse (section "options" osplitfn)))))
 
 (defn docopt
+  "Parse `argv` based on command-line interface described in `doc`.
+
+  `docopt` creates your command-line interface based on its
+  description that you pass as `doc`. Such description can contain
+  --options, <positional-argument>, commands, which could be
+  [optional], (required), (mutually | exclusive) or repeated...
+
+  This function has multiple arities. The default one is the 2-arity
+  one.
+
+  `result-fn` is a function that will be called with the resulting
+  map after parsing `doc`+`argv`. By default in the 2 arity version
+  of this function, this will be set to `identity` function, i.e.:
+  will return the map without changes.
+
+  `usage-fn` is a function that will be called in case of issues
+  while parsing, and will receive the `doc` as argument. By default
+  in the 2/3 arity version of this function, this will be set to
+  `println`, i.e.: it will print the usage of the program and return
+  `nil`."
   ([doc args]
-   (docopt doc args identity println))
-  ([doc args f]
-   (docopt doc args f println))
-  ([doc args f print-fn]
+   (docopt doc args identity))
+  ([doc args result-fn]
+   (docopt doc args result-fn println))
+  ([doc args result-fn usage-fn]
    (if-let [arg-map (-> doc parse (m/match-argv args))]
-     (f arg-map)
-     (print-fn doc))))
+     (result-fn arg-map)
+     (usage-fn doc))))
