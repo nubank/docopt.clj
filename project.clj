09fc4c72db7266a87dbc6aa8909eb4c4332e5879
Thiago Kenji Okada (Nubank) <thiago.okada@nubank.com.br>
2021-05-11 14:58:50 -0300

    Prepare 0.6.1-fix5 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dev.nubank/docopt "0.6.1-fix4"
+(defproject dev.nubank/docopt "0.6.1-fix5"
   :description "docopt creates beautiful command-line interfaces - clojure port"
   :url "http://docopt.org"
   :license {:name "MIT" :url "https://github.com/docopt/docopt.clj/blob/master/LICENSE"}
